b5cc253cb0f0285113440c4c43baac2d8ef940d0
Mike Dougherty <awoodorc@gmail.com>
2017-05-24 17:13:35 -0400

    First commit of seed-custom

---
+++ routes/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/message/:msg', function(req, res, next) {
+  res.render('node', { message: req.params.msg });
+});
+
+router.post('/message', function(req, res, next) {
+  var message = req.body.message;
+  res.redirect('/message/' + message);
+});
+
+module.exports = router;
