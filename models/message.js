fe4ff5cda13a496d915e31e8ef64287e2b08d935
Mike Dougherty <awoodorc@gmail.com>
2017-05-25 10:45:59 -0400

    Setup for angular branch

--- models/message.js
+++ models/message.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 var schema = new Schema({
     content: {type: String, required: true},
     user: {type: Schema.Types.ObjectId, ref: 'User'},
-    //title: {type: String, required: true},
+    title: {type: String, required: true},
 });
 
 module.exports = mongoose.model('Message', schema);
