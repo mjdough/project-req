b10511ca6ad0f2850f7849e9f09d09474f8ea24c
Mike Dougherty <awoodorc@gmail.com>
2017-05-24 20:31:06 -0400

    Refactor model project

--- models/user.js
+++ models/user.js
@@ -7,7 +7,7 @@ var schema = new Schema({
     lastName: {type: String, required: true},
     password: {type: String, required: true},
     email: {type: String, required: true, unique: true},
-    messages: [{type: Schema.Types.ObjectId, ref: 'Message'}]
+    messages: [{type: Schema.Types.ObjectId, ref: 'Project'}]
 });
 
 schema.plugin(mongooseUniqueValidator);
