cde0e0a45d44bbca53a552c016e5774de9d70a13
Mike Dougherty <awoodorc@gmail.com>
2017-05-26 12:53:19 -0400

    Header and nav setup

---
+++ assets/app/auth/signin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+import { User } from "./user.model";
+
+@Component({
+    selector: 'app-signin',
+    templateUrl: './signin.component.html'
+})
+export class SigninComponent {
+
+}
