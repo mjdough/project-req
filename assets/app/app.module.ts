c4af8db44ded4e2ea3a994a06fe2780eaa953ed3
Mike Dougherty <awoodorc@gmail.com>
2017-05-28 12:07:46 -0400

    Put auth and message in modules

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -4,42 +4,33 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from "./app.component";
-import { MessageComponent } from "./messages/message.component";
-import { MessageListComponent } from "./messages/message-list.component";
-import { MessageInputComponent } from "./messages/message-input.component";
-import { MessagesComponent } from "./messages/messages.component";
 import { AuthenticationComponent } from "./auth/authentication.component";
 import { HeaderComponent } from "./header/header.component";
 import { routing } from "./app.routing";
-import { LogoutComponent } from "./auth/logout.component";
-import { SignupComponent } from "./auth/signup.component";
-import { SigninComponent } from "./auth/signin.component";
-import { AuthService } from "./auth/auth.service";
 import { ErrorComponent } from "./errors/error.component";
 import { ErrorService } from "./errors/error.service";
+import { MessageModule } from './messages/message.module';
+import { AuthModule } from "./auth/auth.module";
+import { AuthService } from "./auth/auth.service";
 
 @NgModule({
     declarations: [
         AppComponent,
-        MessageComponent,
-        MessageListComponent,
-        MessageInputComponent,
-        MessagesComponent,
         AuthenticationComponent,
         HeaderComponent,
-        LogoutComponent,
-        SignupComponent,
-        SigninComponent,
         ErrorComponent
     ],
     imports: [
         BrowserModule,
-        FormsModule,
         routing,
-        ReactiveFormsModule,
-        HttpModule
-    ],
-    providers: [ AuthService, ErrorService ],
+        HttpModule,
+        MessageModule,
+        AuthModule
+      ],
+    providers: [
+        ErrorService,
+        AuthService
+      ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
